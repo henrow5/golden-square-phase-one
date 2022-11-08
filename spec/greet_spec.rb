9fae83b0ee89c167fc761029723e50af6873259a
henrychow <113641610+henrow5@users.noreply.github.com>
2022-11-08 12:30:22 +0000

    organising

--- spec/greet_spec.rb
+++
@@ -1,8 +0,0 @@
-require "greet"
-
-RSpec.describe "greet" do
-  it "greets a given user" do
-    result = greet("John")
-    expect(result).to eq "Hello, John"
-  end
-end
--- spec/report_length_spec.rb
+++
@@ -1,8 +0,0 @@
-require "report_length"
-
-RSpec.describe "report length" do
-  it "returns a message with the length of a given string" do
-    result = report_length("orange")
-    expect(result).to eq "This string was 6 characters long."
-  end
-end
